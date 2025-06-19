09685a4c2f640a01cdd75794ad9a8bd4798c18d8
Reymel Sardenia <sardeniareymel@gmail.com>
2025-06-19 15:52:16 +0800

    chore: removed unused file

--- src/app/register/type/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ApplicationTypeSelector } from "@/components/applications/ApplicationTypeSelector";
-
-export default function ApplicationTypePage() {
-  return <ApplicationTypeSelector />;
-}
